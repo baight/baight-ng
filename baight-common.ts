4625b2ea89dd86b19d00a1f9407579f4c3cd8628
chenzheng <baight.chen@ximalaya.com>
2021-04-21 14:24:30 +0800

    优化

--- baight-common.ts
+++ baight-common.ts
@@ -16,6 +16,7 @@ export class BaightCommon {
             "M+": date.getMonth() + 1, //月份 
             "d+": date.getDate(), //日 
             "h+": date.getHours(), //小时 
+            "H+": date.getHours(), //小时 
             "m+": date.getMinutes(), //分 
             "s+": date.getSeconds(), //秒 
             "q+": Math.floor((date.getMonth() + 3) / 3), //季度 
